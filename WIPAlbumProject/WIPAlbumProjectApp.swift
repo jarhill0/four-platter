fb9bd2edfaa9b15b5890c03c535e8191bd045ebd
jarhill0 <git@reeshill.net>
2020-11-17 21:45:49 -0800

    Initial Commit

---
+++ WIPAlbumProject/WIPAlbumProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WIPAlbumProjectApp.swift
+//  WIPAlbumProject
+//
+//  Created by Joseph Rees-Hill on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct WIPAlbumProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
